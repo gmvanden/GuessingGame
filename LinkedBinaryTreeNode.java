34583cc6fbb02a2ee779f0001e513c6163e4a90a
gmvanden <gmvanden@mtu.edu>
2022-12-05 10:46:50 -0500

    did a wee bit of work on the LinkedBinaryTreeNode file getting a few of these methods set-up -- more work to be done here yet

--- LinkedBinaryTreeNode.java
+++ LinkedBinaryTreeNode.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
 
 public class LinkedBinaryTreeNode implements BinaryTreeNode{
+    BinaryTreeNode rootNode = null;
+    BinaryTreeNode parentNode = null;
+    BinaryTreeNode leftNode = null;
+    BinaryTreeNode rightNode = null;
     @Override
     public Object getData() {
         return null;
@@ -13,74 +17,88 @@ public class LinkedBinaryTreeNode implements BinaryTreeNode{
 
     @Override
     public BinaryTreeNode getRoot() {
-        return null;
+        return rootNode;
     }
 
     @Override
     public BinaryTreeNode getParent() {
-        return null;
+        return parentNode;
     }
 
     @Override
-    public void setParent(BinaryTreeNode parent) {
-
-    }
+    public void setParent(BinaryTreeNode parent) { parentNode = parent;}
 
     @Override
     public BinaryTreeNode getLeft() {
-        return null;
+        return leftNode;
     }
 
     @Override
-    public void setLeft(BinaryTreeNode child) {
-
-    }
+    public void setLeft(BinaryTreeNode child) { leftNode = child; }
 
     @Override
     public BinaryTreeNode getRight() {
-        return null;
+        return rightNode;
     }
 
     @Override
-    public void setRight(BinaryTreeNode child) {
-
-    }
+    public void setRight(BinaryTreeNode child) { rightNode = child; }
 
     @Override
     public boolean isParent() {
+        if ((rootNode.hasLeftChild() || rootNode.hasRightChild())) {
+            return true;
+        }
         return false;
     }
-
     @Override
     public boolean isLeaf() {
+        if (!(rootNode.hasLeftChild() || rootNode.hasRightChild())) {
+            return true;
+        }
         return false;
     }
 
     @Override
     public boolean hasLeftChild() {
+        if(rootNode.getLeft() != null){
+            return true;
+        }
         return false;
     }
 
     @Override
     public boolean hasRightChild() {
+        if(rootNode.getRight() != null){
+            return true;
+        }
         return false;
     }
 
     @Override
     public int getDepth() {
-        return 0;
+        if (rootNode == null) return -1;
+        else return 1;
     }
 
     @Override
     public int getHeight() {
-        return 0;
+        return -1;
     }
 
     @Override
     public int size() {
-        return 0;
+        if(rootNode==null){
+            return 0;
+        }
+        return 1 + size(rootNode.getLeft()) + size(rootNode.getRight());
     }
-
+    public int size(BinaryTreeNode node) {
+        if(node==null){
+            return 0;
+        }
+        return 1 + size(node.getLeft()) + size(node.getRight());
+    }
     @Override
     public void removeFromParent() {
 
@@ -88,7 +106,9 @@ public class LinkedBinaryTreeNode implements BinaryTreeNode{
 
     @Override
     public ArrayList<BinaryTreeNode> pathTo(BinaryTreeNode descendant) {
-        return null;
+        ArrayList<BinaryTreeNode> descendantNodes = new ArrayList<>();
+
+        return descendantNodes;
     }
 
     @Override
