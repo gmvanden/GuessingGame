da7877e11f730c785ce5cb922ce22b4bf148910d
gmvanden <gmvanden@mtu.edu>
2022-12-07 15:13:36 -0500

    continued work and fixed a typo in tree2.data

--- GuessingGame.java
+++ GuessingGame.java
@@ -16,14 +16,16 @@ public class GuessingGame implements Game{
             Scanner fIn = new Scanner(file);
             while(fIn.hasNext()){
                 String Line = fIn.nextLine();
-                if(Line.substring(0) == "Q"){
+                if(Line.charAt(0) == 'Q'){
                     String nodeName = Line.substring(2,Line.length());
                     LinkedBinaryTreeNode<String> node = new Question<>(nodeName);
+                    //set left and right nodes if they are not null
                     tree.add(node);
                 }
-                if(Line.substring(0) == "Q"){
+                if(Line.charAt(0) == 'G'){
                     String nodeName = Line.substring(2,Line.length());
                     LinkedBinaryTreeNode<String> node = new Guess<>(nodeName);
+                    //set left and right nodes if they are not null
                     tree.add(node);
                 }
             }
@@ -50,7 +52,7 @@ public class GuessingGame implements Game{
 
     @Override
     public BinaryTreeNode<String> getRoot() {
-        return null;
+        return tree.get(0);
     } //do this second
 
     @Override
@@ -64,11 +66,57 @@ public class GuessingGame implements Game{
         }
         while(playing){
             //load tree
+            loadTree(fileName);
+            int iterativeI=0;
             while(Questioning) {
                 //establish question using root and branches
+                //set left side
+                if(tree.size() > iterativeI+2){
+                    if ((tree.get(iterativeI + 1) == null)) {
+                        tree.get(iterativeI).setLeft(null);
+                    } else {
+                        tree.get(iterativeI).setLeft(tree.get(iterativeI + 1));
+                    }
+                    //set right side
+                    if ((tree.get(iterativeI + 2) == null)) {
+                        tree.get(iterativeI).setRight(null);
+                    } else {
+                        tree.get(iterativeI).setRight(tree.get(iterativeI + 2));
+                    }
+                }
                 //output question
+                System.out.println(tree.get(iterativeI).getData().toString());
                 //input answer
+                String response = in.nextLine();
                 //evaluate answer
+
+                //ask question, if useranswer == answer, computer wins
+                // if useranswer != answer, ask the next question
+                if(response.contains("y")){ //no to the left, yes to the right
+                    //update iterativeI -- need to figure out how to up it accurately -- something with checking left and right children
+                    //continue down the tree until reach leaf
+                    if(tree.get(iterativeI).isLeaf()){
+                        System.out.println("I win!");
+                        break;
+                    }
+                } else if (response.contains("n")){//no to the left, yes to the right
+                    //update iterativeI
+                    //need to check if this node is a leaf
+                    //if leaf, then say "it is a *guess*".
+                    //else, then ask question
+
+                    if(tree.get(iterativeI).isLeaf()){
+                        System.out.println("Are you thinking of a "+tree.get(iterativeI).getData().toString().toLowerCase()+"? (y/n)");
+                    } else {
+
+                    }
+
+
+                    System.out.println("Question: "+tree.get(iterativeI).getData()+"(y/n)");
+                }
+
+                iterativeI++;
+
                 //determine continuation, victory or loss
 
                 // if loss or victory is determined, break loop
@@ -79,7 +127,7 @@ public class GuessingGame implements Game{
             //save tree
             System.out.println("would you like to play again?(y/n)\n"); // make sure this is always the last thing to run
             if(in.nextLine().contains("n")){
-                playing = false;
+                saveTree(fileName+"ll");
                 break;
             }
         }
